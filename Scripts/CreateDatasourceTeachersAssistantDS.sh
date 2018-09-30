9945a4e04b8eed40fc00cf4e1aca92b14e0ee676
Michael Zuercher <michzuerch@gmail.com>
2018-09-30 20:26:18 +0200

    Sunday

--- Scripts/CreateDatasourceTeachersAssistantDS.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-/java/wildfly/bin/jboss-cli.sh --file=/java/Source/TeachersAssistant/Scripts/CreateDatasourceTeachersAssistantDS.cli
--- Scripts/CreateSecurityDomain.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-/bin/cp /java/Source/LehrerVerwaltung/usersLehrerVerwaltung.properties /java/wildfly/standalone/configuration/usersLehrerVerwaltung.properties
-/bin/cp /java/Source/LehrerVerwaltung/rolesLehrerVerwaltung.properties /java/wildfly/standalone/configuration/rolesLehrerVerwaltung.properties
-
-/java/wildfly/bin/jboss-cli.sh --file=/java/Source/LehrerVerwaltung/Scripts/CreateSecurityDomain.cli
